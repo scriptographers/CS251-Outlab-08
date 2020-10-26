cdb702dd9a9280c7f57afe82c7ae12ac39810e48
devansh-dvj <devansh.dvj@gmail.com>
2020-10-26 15:11:51 +0530

    Added PS and resources

---
+++ Resources/P4/StreaminApp.java
@@ -0,0 +1,16 @@
+/*
+DO NOT MODIFY THE CODE STUB
+NO NEED TO DEFINE main()
+*/
+
+import java.util.*;
+
+class StreamingApp
+{
+	public static Map<String, ArrayList<String> > getFavouriteGenres(Map<String, ArrayList<String> > userMovies, Map<String, ArrayList<String> > movieGenres)
+	{
+		
+		// WRITE YOUR CODE HERE
+
+	}
+}
