0f0f3577619c2d482de9ffc60b73dc609af1655f
Harshit Varma <varmaharshit2@gmail.com>
2020-11-02 21:01:57 +0530

    P4: Task B done using single for loop

---
+++ P4/Sieve.java
@@ -0,0 +1,22 @@
+import java.util.*;  
+import java.util.stream.IntStream; 
+import java.util.stream.Collectors;
+
+public class Sieve{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        n += 1; // if n is prime, it gets printed too
+
+        List<Integer> nums = IntStream.range(2, n).boxed().collect(Collectors.toList());
+        List<Integer> divisors = IntStream.range(2, (int)Math.ceil(Math.sqrt(n))).boxed().collect(Collectors.toList());
+
+        for (int i = 0; i < divisors.size(); i++) {
+            int p = divisors.get(i);
+            nums = nums.stream().filter(x -> (x%p > 0 || x==p)).collect(Collectors.toList());
+        }
+
+        System.out.println(nums.toString().replaceAll("[,\\[\\]]", ""));
+    }
+}
