66075922b8c27ae9349da8c0e9a079ea4f3ea816
Harshit Varma <varmaharshit2@gmail.com>
2020-11-04 17:21:12 +0530

    A3: Completed without any loops

--- P4/Sieve.java
+++ P4/Sieve.java
@@ -8,14 +8,14 @@ public class Sieve{
         int n = sc.nextInt();
 
         n += 1; // if n is prime, it gets printed too
-
+        
+        // Create potential primes 
         List<Integer> nums = IntStream.range(2, n).boxed().collect(Collectors.toList());
+        // Create potential divisors
         List<Integer> divisors = IntStream.range(2, (int)Math.ceil(Math.sqrt(n))).boxed().collect(Collectors.toList());
-
-        divisors.stream().forEach(p -> {
-            nums.removeIf(x -> (x%p == 0 && x!=p));
-        });
-
+        // nums.removeIf returns an a boolean value, convert it into a 0 always, and use map, collect after that
+        divisors.stream().map(p -> nums.removeIf(x -> (x%p == 0 && x!=p))?0:0).collect(Collectors.toList());
+        // Print final list, replace all '[', ',' and ']' by ""
         System.out.println(nums.toString().replaceAll("[,\\[\\]]", ""));
     }
 }
