7b92efe6ac174a045514967292ca98c61a4b9ced
Harshit Varma <varmaharshit2@gmail.com>
2020-11-04 17:40:29 +0530

    P4: Improved Task-B code

--- P4/Sieve.java
+++ P4/Sieve.java
@@ -8,13 +8,13 @@ public class Sieve{
         int n = sc.nextInt();
 
         n += 1; // if n is prime, it gets printed too
-        
+
         // Create potential primes 
         List<Integer> nums = IntStream.range(2, n).boxed().collect(Collectors.toList());
-        // Create potential divisors
-        List<Integer> divisors = IntStream.range(2, (int)Math.ceil(Math.sqrt(n))).boxed().collect(Collectors.toList());
-        // nums.removeIf returns an a boolean value, convert it into a 0 always, and use map, collect after that
-        divisors.stream().map(p -> nums.removeIf(x -> (x%p == 0 && x!=p))?0:0).collect(Collectors.toList());
+        
+        // Filter nums based on divisors
+        IntStream.range(2, (int)Math.ceil(Math.sqrt(n))).boxed().map(p -> nums.removeIf(x -> (x%p == 0 && x!=p))?0:0).collect(Collectors.toList());
+        
         // Print final list, replace all '[', ',' and ']' by ""
         System.out.println(nums.toString().replaceAll("[,\\[\\]]", ""));
     }
